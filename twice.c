8ebffb86e15e18b3af4364e5f3f9b2375f49daaf
RagaviAmutha <ragaviamu@gmail.com>
2016-06-09 11:20:21 +0530

    Create twice.c

---
+++ twice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,i;
+	int a[100],hash[10]={0};
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+		scanf("%d",&a[i]);
+	for(i=0;i<t;i++)
+		hash[a[i]]++;
+	for(i=0;i<t;i++)
+	{
+			if(hash[i]!=2)
+			{
+				printf("%d",i);
+			break;
+			}
+	}
+	return 0;
+}
