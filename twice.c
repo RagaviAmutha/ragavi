ef2884a9f77b32b114215258708ea4b332915c3a
RagaviAmutha <ragaviamu@gmail.com>
2016-06-09 12:07:59 +0530

    Update twice.c

--- twice.c
+++ twice.c
@@ -5,15 +5,15 @@ int main(void) {
 	int t,i,max=0;
 	int a[100],hash[10000]={0};
 	scanf("%d",&t);
-	for(i=0;i<t;i++)
+	for(i=0;i<=t;i++)
 	{
 		scanf("%d",&a[i]);
 		if(max<a[i])
 			max=a[i];
 	}
-	for(i=0;i<max;i++)
+	for(i=0;i<=max;i++)
 		hash[a[i]]++;
-	for(i=0;i<max;i++)
+	for(i=0;i<=max;i++)
 	{
 			if(hash[i]==1)
 			{
