ac021a7fb628dc611e6b04a1fc5c4549d3bc2949
RagaviAmutha <ragaviamu@gmail.com>
2016-06-09 12:06:31 +0530

    Update unique.c

--- unique.c
+++ unique.c
@@ -2,14 +2,18 @@
 
 int main(void) {
 	// your code goes here
-	int t,i;
-	int a[100],hash[10]={0};
+	int t,i,max=0;
+	int a[100],hash[10000]={0};
 	scanf("%d",&t);
 	for(i=0;i<t;i++)
+	{
 		scanf("%d",&a[i]);
-	for(i=0;i<t;i++)
+		if(max<a[i])
+			max=a[i];
+	}
+	for(i=0;i<=max;i++)
 		hash[a[i]]++;
-	for(i=0;i<t;i++)
+	for(i=0;i<=max;i++)
 	{
 			if(hash[i]==1)
 				printf("%d",i);
