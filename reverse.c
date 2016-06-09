759d213cfc3f4dcf6df9ec311b75131184e055ef
RagaviAmutha <ragaviamu@gmail.com>
2016-06-09 16:27:49 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,16 @@
+
+copy
+
+    #include <stdio.h>
+    #include<string.h>
+    int main(void) {
+    	// your code goes here
+    	char str[100];
+    	int i,len;
+    	scanf("%s",str);
+    	len=strlen(str)-1;
+    	for(i=len;i>=0;i--)
+    		printf("%c",str[i]);
+    	return 0;
+    }
+     
