d39476bd206479edf097da033ab1ce712cb2def2
RagaviAmutha <ragaviamu@gmail.com>
2016-06-09 12:07:27 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+int cmp(const void *a,const void *b){
+    return (*(int*)a)-(*(int*)b);
+}
 int main(void) {
 	
 	int t,index;
@@ -7,6 +9,7 @@ int main(void) {
 	scanf("%d",&t);
 	for(i=0;i<t;i++)
 		scanf("%d",&a[i]);
+	 qsort(a,n,sizeof(int),cmp);
 	scanf("%d",&index);
 	for(i=0;i<t;i++)
 	{
