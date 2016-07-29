6defdea3c9bd88019e939fc815fdf97b57e721ab
RagaviAmutha <ragaviamu@gmail.com>
2016-07-29 21:15:48 +0530

    Create first_occur.c

---
+++ first_occur.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	// your code goes here
+	int t,i,j,n,max;
+	char a[100][100];
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+		scanf("%s",a[i]);
+	max=strlen(a[0]);
+	for(i=0;i<t;i++)
+	{
+		n=strlen(a[i]);
+		if(n<max)
+			max=n;
+	}
+	printf("%d",max);
+	return 0;
+}
