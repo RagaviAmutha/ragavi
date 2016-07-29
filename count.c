7aa52d39bc487f8b99225ecd9aa663828e5af954
RagaviAmutha <ragaviamu@gmail.com>
2016-07-30 01:26:10 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,max=999;
+	scanf("%d",&t);
+	int a[100],i,d,d1,j;
+	for(i=0;i<t;i++)
+		scanf("%d",&a[i]);
+	scanf("%d %d",&d,&d1);
+	for(i=0;i<t;i++)
+	{
+		int count=0;
+		if(a[i]==d)
+		{
+			for(j=i;j<t;j++)
+			{
+				if(a[j]==d1)
+					break;
+				count++;
+			}
+		}
+		if(count<max&&count!=0)
+			max=count;
+		//printf("%d",count);
+	}
+	printf("%d",max);
+
+	return 0;
+}
