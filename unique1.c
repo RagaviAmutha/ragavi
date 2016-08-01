9db25ab2465ad3500f276e05ad61c636a9fd52ae
RagaviAmutha <ragaviamu@gmail.com>
2016-08-01 22:08:52 +0530

    Update unique1.c

--- unique1.c
+++ unique1.c
@@ -8,13 +8,14 @@
     		scanf("%d",&a[i]);
     		if(a[i]>0)
     		hash[a[i]]++;
+    	
+    	}
+    	for(i=0;i<n;i++)
+    	{
+    		if(hash[a[i]]>0)
+    		printf("%d",a[i]);
     		if(a[i]<0)
     		    printf("%d".a[i]);
-    	}
-    	for(i=0;i<9;i++)
-    	{
-    		if(hash[i]==1)
-    		printf("%d",i);
     	}
      
      
