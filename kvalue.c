16f391181dd88127ca70f9005ba087bb2ec7c60b
RagaviAmutha <ragaviamu@gmail.com>
2016-08-02 21:45:24 +0530

    Create kvalue.c

---
+++ kvalue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,a[100],i,n,count=0;
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+		scanf("%d",&a[i]);
+	n=a[t-1];
+	for(i=0;i<t;i++)
+	{
+		if(a[i]>n)
+			count++;
+	}
+	printf("%d",(t-count)-1);
+	return 0;
+}
