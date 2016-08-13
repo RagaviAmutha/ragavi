245fbd7bac6befb5e87ebe587b674e6ec2dabe0a
RagaviAmutha <ragaviamu@gmail.com>
2016-08-13 21:43:28 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char a;
+	scanf("%c",&a);
+	if(a=='a'||a=='e'||a=='i'||a=='e'||a=='u')
+		printf("Vowels");
+	else
+		printf("Constants");
+	return 0;
+}
